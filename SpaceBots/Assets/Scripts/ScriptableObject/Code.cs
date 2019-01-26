e971edb332329651b9cf41331103ff276c424fb9
Kamau Vassall <kamau.vassall@gmail.com>
2019-01-26 13:36:40 -0600

    Under the Hood - Make some or all of the code visible in your game

---
+++ SpaceBots/Assets/Scripts/ScriptableObject/Code.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Code", menuName = "ScriptableObjectData/Code", order = 2)]
+public class Code : ScriptableObject
+{
+	public string text;
+	public int scrollSpeed;
+}
---
+++ SpaceBots/Assets/Scripts/UI/CodeRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class CodeRenderer : MonoBehaviour
+{
+	#region Enums and Constants
+
+	#endregion
+
+	#region Events
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Inspectables
+
+	public Code code;
+	public Text text;
+
+	#endregion
+
+	#region Private Member Variables
+
+	private RectTransform m_TextTransform;
+	private Vector3 m_InitialPosition;
+
+	#endregion
+
+	#region Monobehaviours
+
+	protected void Start()
+	{
+		text.text = code.text;
+		m_TextTransform = text.rectTransform;
+		m_InitialPosition = m_TextTransform.localPosition;
+	}
+
+	protected void Update()
+	{
+		//m_TextTransform.localPosition.y += code.scrollSpeed;
+		m_TextTransform.localPosition = new Vector3(m_TextTransform.localPosition.x, m_TextTransform.localPosition.y + code.scrollSpeed);
+		if (m_TextTransform.localPosition.y > 1000) {
+			m_TextTransform.localPosition = m_InitialPosition;
+		}
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
