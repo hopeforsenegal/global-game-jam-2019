a8e58390d491a98db60679a54fdd4e944e703521
Kamau Vassall <kamau.vassall@gmail.com>
2019-01-26 00:38:44 -0600

    Transitioning between the various scenes is working like a smooth baby

---
+++ SpaceBots/Assets/Scripts/TestContinue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestContinue : MonoBehaviour
+{
+	public Settings settings;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var hitEnterKey = Input.GetKey(KeyCode.KeypadEnter)
+			|| Input.GetKey(KeyCode.Return);
+
+		var hitEscKey = Input.GetKey(KeyCode.Escape);
+
+		if (hitEnterKey) {
+			NextScene();
+		}
+	}
+
+	void NextScene()
+	{
+		GameController.Instance.sceneIndex++;
+		UnityEngine.SceneManagement.SceneManager.LoadScene(settings.scenes[GameController.Instance.sceneIndex].sceneToLoad);
+	}
+}
