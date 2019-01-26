455c25ff44cbd3f3895f3058d12497d2c0db9975
Kamau Vassall <kamau.vassall@gmail.com>
2019-01-26 12:26:24 -0600

    Made it so that you can no longer move a previously locked piece

--- SpaceBots/Assets/Scripts/PuzzlePiece.cs
+++ SpaceBots/Assets/Scripts/PuzzlePiece.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class PuzzlePiece : MonoBehaviour
 {
+	enum State
+	{
+		Free,
+		Moving,
+		ReadyToLock,
+		Locked
+	}
+
 	public static event Action PickupEvent;
 
 	public static event Action DropCorrectEvent;
@@ -12,148 +20,136 @@ public class PuzzlePiece : MonoBehaviour
 	public static event Action DropWrongEvent;
 
 	public static event Action RotateEvent;
-	
-    //use to track number of pieces
-    public int id;
 
-    //if you can spawn a piece
-    public bool Spawn;
 
-    private GameObject Parent;
+	private static int sNumPieces;
 
-    private Vector3 lockPos;
 
-    //if obj is in place
-    private int locked;
+	//use to track number of pieces
+	public int id;
 
-    //if obj is stationary ref for copy
-    public bool slot;
+	//if you can spawn a piece
+	public bool Spawn;
 
-    //number of pieces left
-    [SerializeField]
-    private int numPieces;
+	//if obj is stationary ref for copy
+	public bool slot;
 
-    //max number of objects this can spawn
-    [SerializeField]
-    private int MaxPieces;
+	//max number of objects this can spawn
+	[SerializeField]
+	private int MaxPieces;
 
-    private void Start()
-    {
-        if (slot)
-        {
-            Spawn = true;
-            numPieces = MaxPieces;
-        }
-    }
+	private GameObject Parent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!slot)
-        {
-            if (locked != 3)
-            {
-                doMouseThing();
-            }
-        }
+	private Vector3 lockPos;
 
-    }
+	//if obj is in place
+	private State locked;
 
-    void doMouseThing()
-    {
-        if (Input.GetMouseButtonUp(0))
-		{
-			Debug.Log("Sending RayCast");
-            {
-                if (locked == 0)
-				{
+	#region Monobehaviours
+
+	protected void Start()
+	{
+		if (slot) {
+			Spawn = true;
+			sNumPieces = MaxPieces;
+		}
+	}
+
+	protected void Update()
+	{
+		if (!slot) {
+			if (locked != State.Locked) {
+				doMouseThing();
+			}
+		}
+	}
+
+	#endregion
+
+	void doMouseThing()
+	{
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.LogFormat("[{0}/{1}:doMouseThing] Sending RayCast for up. state:{2}", name, id, locked);
+			{
+				if (locked == State.Free) {
 					var invokeEvent = DropCorrectEvent;
 					if (invokeEvent != null) {
 						invokeEvent();
 					}
 
-                    Parent.GetComponent<PuzzlePiece>().AddPiece();
-                    Destroy(this.gameObject);
-                }
-                else if (locked == 1)
-				{
+					Parent.GetComponent<PuzzlePiece>().AddPiece();
+					Destroy(this.gameObject);
+				} else if (locked == State.ReadyToLock) {
 					var invokeEvent = DropWrongEvent;
 					if (invokeEvent != null) {
 						invokeEvent();
 					}
 
-                    locked = 3;
-                    this.gameObject.transform.position = lockPos;
-                }
-            }
-        }
-        else if (Input.GetMouseButton(0))
-		{
-			Debug.Log("Sending RayCast");
-            if (locked == 0 || locked == 1)
-            {
-                Debug.Log(id + " is Moving to mouse");
-                this.gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
-            }
-        }
-    }
+					locked = State.Locked;
+					this.gameObject.transform.position = lockPos;
+					Debug.LogFormat("[{0}/{1}:doMouseThing] is locked", name, id);
+				}
+			}
+		} else if (Input.GetMouseButton(0)) {
+			Debug.LogFormat("[{0}/{1}:doMouseThing] Sending RayCast for hold. state:{2}", name, id, locked);
+			if (locked != State.Locked) {
+				Debug.LogFormat("[{0}/{1}:doMouseThing] is moving", name, id);
+				this.gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+			}
+		}
+	}
 
-    //use for drop location
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Anchor")
-        {
-            locked = 1;
-            lockPos = other.gameObject.transform.position - new Vector3(0,0,1);
-        }
-    }
+	//use for drop location
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Anchor") {
+			Debug.LogFormat("[{0}/{1}:OnTriggerEnter] locked", name, id);
+			// Make into position to show above the lock
+			lockPos = other.gameObject.transform.position - new Vector3(0, 0, 1);
+			locked = State.ReadyToLock;
+		}
+	}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Anchor")
-        {
-            locked = 0;
-            //lockPos = Parent.transform.position;
-        }
-    }
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Anchor") {
+			if (locked != State.Locked) {
+				Debug.LogFormat("[{0}/{1}:OnTriggerExit] unlocked", name, id);
+				locked = State.Moving;
+			}
+		}
+	}
 
-    //use for setting parent piece
-    public void SetParent(GameObject Par)
-    {
-        Parent = Par;
-    }
+	//use for setting parent piece
+	public void SetParent(GameObject Par)
+	{
+		Parent = Par;
+	}
 
-    public void AddPiece()
-    {
-        if(numPieces < MaxPieces)
-        {
-            Debug.Log("Adding Piece");
-            numPieces++;
-            Spawn = true;
-        }
-        else
-        {
-            Debug.Log("Cant Add Piece");
-        }
-    }
+	public void AddPiece()
+	{
+		if (sNumPieces < MaxPieces) {
+			Debug.Log("Adding Piece");
+			sNumPieces++;
+			Spawn = true;
+		} else {
+			Debug.Log("Cant Add Piece");
+		}
+	}
 
-    public void SubPiece()
-    {
-        if (numPieces <= MaxPieces && numPieces > 0)
-        {
-            id = numPieces;
-            numPieces--;
-            Debug.Log("Subtracting Piece");
-            if (numPieces <= 0)
-            {
+	public void SubPiece()
+	{
+		if (sNumPieces <= MaxPieces && sNumPieces > 0) {
+			id = sNumPieces;
+			sNumPieces--;
+			Debug.Log("Subtracting Piece");
+			if (sNumPieces <= 0) {
 
-                Spawn = false;
-                Debug.Log("Cannot spawn any more pieces");
-            }
-        }
-        else
-        {
-            Debug.Log("Cant Sub Piece");
-        }
-    }
+				Spawn = false;
+				Debug.Log("Cannot spawn any more pieces");
+			}
+		} else {
+			Debug.Log("Cant Sub Piece");
+		}
+	}
 }
