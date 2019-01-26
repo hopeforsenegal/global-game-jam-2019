f73b27397f6d98e6c74f085cd9608946f08f50ec
Kamau Vassall <kamau.vassall@gmail.com>
2019-01-25 22:51:29 -0600

    Gettings starts by adding in some template scriptable objects

---
+++ SpaceBots/Assets/Scripts/ScriptableObject/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Settings", menuName = "ScriptableObjectData/Settings", order = 1)]
+public class Settings : ScriptableObject
+{
+	[Header("UI")]
+	[Tooltip("the main menu background")]
+	public Image mainMenuBackground;
+
+	[Header("General")]
+	[Tooltip("the main game scene")]
+	public string gameScene;
+}
---
+++ SpaceBots/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class MainMenu : MonoBehaviour
+{
+	public Image background;
+	public string sceneName = "main_scene";
+
+	// Use this for initialization
+	void Start()
+	{
+		background.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var hitEnterKey = Input.GetKey(KeyCode.KeypadEnter)
+			|| Input.GetKey(KeyCode.Return)
+							   || Input.GetKey((KeyCode.Space));
+
+		var hitLetter = false;
+		foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode))) {
+			if (kcode >= KeyCode.A && kcode <= KeyCode.Z && Input.GetKeyDown(kcode)) {
+				Debug.Log("KeyCode down: " + kcode);
+				hitLetter = true;
+			}
+		}
+
+		if (hitEnterKey || hitLetter) {
+			PlayGame();
+		} else if (Input.GetKey(KeyCode.Escape)) {
+			ExitGame();
+		}
+	}
+
+	protected void OnMouseDown()
+	{
+		PlayGame();
+	}
+
+	private void PlayGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+	}
+
+	private void ExitGame()
+	{
+#if UNITY_EDITOR
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
